ca3fb0c110272d2f67c5c7c51813dd8cce8b7895
Nikhil Ahirwar <nikhilraj2908@gmail.com>
2025-08-18 16:56:12 +0530

    changed server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const app = require('./app');
 const connectDB = require('./config/db');
 
 connectDB();
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
